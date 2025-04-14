3687dffa8e52023c6dbb0f0eed8085618875fad5
vasitha1 <sulemfelsi@gmail.com>
2025-04-14 21:28:28 +0000

    Task 1: Running lint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    ignores: ['node_modules/**'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      // Your rules here
+    }
+  }
+];
