9cf882b9ec5494f424c4da97084940e3fae3cbd1
vasitha1 <sulemfelsi@gmail.com>
2025-04-16 21:43:42 +0000

    Trying task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,23 +1,10 @@
-// Step 1: Importing the functions from utils.js  
-import { uploadPhoto, createUser } from './utils.js';  
+import { uploadPhoto, createUser } from './utils';
 
-// Step 2: Defining the handleProfileSignup function  
-async function handleProfileSignup() {  
-  try {  
-    // Step 3: Resolving both promises collectively  
-    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);  
-
-    // Step 4: Logging firstName and lastName to the console  
-    console.log(`Body: ${user.firstName} ${user.lastName}`);  
-    
-    // Optionally log the photo response if needed  
-    // console.log(photo);  
-    
-  } catch (error) {  
-    // Step 5: Handling any errors  
-    console.error('Signup system offline');  
-  }  
-}  
-
-// Assuming this function is called somewhere in your code  
-handleProfileSignup(); 
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
