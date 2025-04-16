af78554a6049b99f2380e33519353984699a9419
vasitha1 <sulemfelsi@gmail.com>
2025-04-16 22:07:24 +0000

    Task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
+}
