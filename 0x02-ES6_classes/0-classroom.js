8efa6e5697b7783a3deabb16ecc9c26109eecea8
vasitha1 <sulemfelsi@gmail.com>
2025-04-17 18:53:13 +0000

    Fixed lint task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,12 +1,12 @@
-/**  
- * ClassRoom  
- */  
-export default class ClassRoom {  
-    /**  
-     * Creates a new ClassRoom instance with a maximum number of students.  
-     * @param {number} maxStudentsSize - The maximum number of students in the class.  
-     */  
-    constructor(maxStudentsSize) {  
-      this._maxStudentsSize = maxStudentsSize; // Store maxStudentsSize in a private variable  
-    }  
-  }  
+/**
+ * ClassRoom
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new ClassRoom instance with a maximum number of students.
+   * @param {number} maxStudentsSize - The maximum number of students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize; // Store maxStudentsSize in a private variable
+  }
+}
