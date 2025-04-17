2838ccd86b3665ab5a8dd2a02182c599ec859800
vasitha1 <sulemfelsi@gmail.com>
2025-04-17 18:44:31 +0000

    Task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,12 +1,12 @@
-/**
- *  class room.
- */
-export default class ClassRoom {
-/**
- * Creates a new - The maximum number of
- * students in the class.
- */
-  constructor(maxStudentsSize) {
-    this.maxStudentsSize = maxStudentsSize;
-  }
-}
+/**  
+ * ClassRoom  
+ */  
+export default class ClassRoom {  
+    /**  
+     * Creates a new ClassRoom instance with a maximum number of students.  
+     * @param {number} maxStudentsSize - The maximum number of students in the class.  
+     */  
+    constructor(maxStudentsSize) {  
+      this._maxStudentsSize = maxStudentsSize; // Store maxStudentsSize in a private variable  
+    }  
+  }  
