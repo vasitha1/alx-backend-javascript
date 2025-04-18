5a9254abf0067f2b591a836caca1dd1cd8d3abf4
vasitha1 <sulemfelsi@gmail.com>
2025-04-18 10:21:33 +0000

    Refactoring 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,11 +1,4 @@
-/**
- * ClassRoom
- */
 export default class ClassRoom {
-  /**
-   * Creates a new ClassRoom instance with a maximum number of students.
-   * @param {number} maxStudentsSize - The maximum number of students in the class.
-   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
