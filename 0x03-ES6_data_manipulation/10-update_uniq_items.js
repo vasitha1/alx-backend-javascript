528394e6c27932c99195aa5ccbf8fa85d567fa41
vasitha1 <sulemfelsi@gmail.com>
2025-04-22 21:36:13 +0000

    Task 10, 11, 100

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(items) {
+  if (!(items instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const [k, v] of items.entries()) {
+    if (v === 1) {
+      items.set(k, 100);
+    }
+  }
+  return items;
+}
