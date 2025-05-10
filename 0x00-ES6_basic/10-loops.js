f43a97b5848dd888fbb18fa98dddd92f3aed1f1c
vasitha1 <sulemfelsi@gmail.com>
2025-05-10 15:15:18 +0000

    refactoring 10-loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
   for (const value of array) {
-    const idx = array.indexOf(value);
-    array[idx] = appendString + value;
+    result.push(appendString + value);
   }
-
-  return array;
+  return result;
 }
